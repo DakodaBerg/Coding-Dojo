f548a1417ed965f333fbc6ea0f5752ce4780e7d1
Dakoda Berg <koda3927@gmail.com>
2022-02-22 17:18:52 -0500

    organized files

---
+++ Pre-Bootcamp/Berg_Dakoda_JS_Tinkering.js
@@ -0,0 +1,11 @@
+var childHeight = 40;
+function displayifChildisAbleToRideTheRollerCoaster('') {
+    if ( var childheight >= 52; print("Get on that ride, kiddo!"){
+        else (print ("Sorry kiddo. Maybe next year.")
+        
+    }
+}
+
+console.log(displayifChildisAbleToRideTheRollerCoaster)
+
+
